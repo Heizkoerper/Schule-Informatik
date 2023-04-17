47946f1dbbbe85c3458edd06b9d3f19b14f0ba3d
Jonathan Schnieders <110174069+Heizkoerper@users.noreply.github.com>
2023-04-17 15:28:59 +0000

    Initial Commit

--- Kartenspiel/Kartenspiel.java
+++ Kartenspiel/Kartenspiel.java
@@ -23,12 +23,14 @@ public class Kartenspiel {
 
     }
 
-
+    /**
+    * Mischt die Karten indem es 32 mal zwei zufaellige Karten vertauscht
+    **/
     public void mischen() {
 
         for(int i = 0; i > 32; i++) {
 
-            // Erstelle zwei zufällige Integer zwischen 0 und 31
+            // Erstelle zwei zufaellige Integer zwischen 0 und 31
             int a = (int) Math.random() * 31;
             int b = (int) Math.random() * 31;
 
@@ -40,13 +42,56 @@ public class Kartenspiel {
 
     }
 
-
+    /**
+    * Tauscht zwei Karten aus dem Kartendeck
+    * @param a : int - Index der ersten zu tauschenden Karte im Array
+    * @param b : int - Index der zweiten zu tauschenden Karte im Array
+    **/
     public void tauschen(int a, int b) {
 
         Karte temp = karten[a];
 
         karten[a] = karten[b];
         karten[b] = temp;
+
+    }
+
+
+
+    /**
+    * Diese Methode enfternt alle Karten einer vorgegebenen Farbe aus dem Kartenspiel
+    * @param pFarbe : String - Die Farbe, welche entfernt werden soll
+    **/
+    public void aussortieren(String pFarbe) {
+
+        for (int i = 0; i < Kartendeck.length; i++) {
+
+            if (Kartendeck[i].getFarbe().equals(pFarbe)) {
+                Kartendeck[i] = null;
+            }
+
+        }
+
+    }
+
+
+    /**
+     * Überprüft, ob sich eine bestimmte Karte (spezifiziert durch Farbe und Bild) im Kartendeck befindet.
+     * @param pFarbe Die vorgegebene Farbe
+     * @param pBild Das vorgegebene Bild
+     * @return Wahrheitswert der evaluation ob die Karte im Kartendeck vorhanden ist.
+     */
+    public boolean vorhanden(String pFarbe, String pBild) {
+
+        for (Karte karte : Kartendeck) {
+
+            if (karte.getFarbe().equals(pFarbe) && karte.getBild().equals(pBild)) {
+                return true;
+            }
+
+        }
+
+        return false;
 
     }
 
